eb12d78ca17c01df2e631941b684e7b8ad40dacc
djyugg <sv03@n741x1.com>
2015-01-09 01:46:56 +0900

    ExcelVBAモジュールエクスポートツール 作りかけ

--- excel_vba/excel_vba_export.ps1
+++ excel_vba/excel_vba_export.ps1
@@ -9,30 +9,64 @@ Get-ChildItem -Recurse -Include *.xls,*.xlsx | ForEach-Object {
 function export_vba($file_name){
     $file_path = Join-Path $PWD $file_name
 
-    echo $file_path
-    
     $excel = new-object -ComObject Excel.Application
 
     $excel.Workbooks.Open($file_path) | % {
         $_.VBProject.VBComponents | % {
-
+         
+            $file_path = create_export_file_name $_ $file_name
         }
+    }
+
+    $excel.Quit()
+}
+
+function create_export_directory($file_name) {
+    $root_directory_name = 
+    $directory = Join-Path $PWD $file_name
+    
+
+    $directories = @("module", "class", "form", "document_module")
+
+    foreach($directories in $directory) {
+
     }
 
 }
 
 
-function create_export_file_name($vb_component) {
+function create_export_file_name($vb_component, $export_target_file) {
 
     switch($vb_component.Type) 
     {
-        1{ $extension = ".bas"}
-        2{ $extension = ".cls"}
-        3{ $extension = ".frm"}
-        11 { $extension = "" }
-        100{ $extension = ".bas"}
+        1{ 
+            $extension = ".bas"
+            $directory = "module"
+        }
+        2{ 
+            $extension = ".cls"
+            $directory = "class"
+        }
+        3{
+            $extension = ".frm"
+            $directory = "form"
+        }
+        # TODO ActiveX Object
+        11{
+            $extension = ""
+            $directory = "module"
+        }
+        100{
+            $extension = ".cls"
+            $directory = "dosument_module"
+        }
     }
+    $root_directory = [System.IO.Path]::GetFileNameWithoutExtension($export_target_file)
 
+    $create_directory = Join-Path $PWD $root_directory
+    $create_directory = Join-Path $create_directory $directory
+    $file_name = $vb_component.Name + $extension
+    $file_path = Join-Path $create_directory $file_name
 
-    return [string]::Join($vb_component.Name, $extension)
+    return $file_path
 }
