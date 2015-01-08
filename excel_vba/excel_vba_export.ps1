abacbbe0d45f534175191ec5f7c58baba32b1e46
djyugg <sv03@n741x1.com>
2015-01-08 23:58:56 +0900

    ExcelVBAモジュールエクスポートツール 作りかけ

--- excel_vba/excel_vba_export.ps1
+++ excel_vba/excel_vba_export.ps1
@@ -7,6 +7,32 @@ Get-ChildItem -Recurse -Include *.xls,*.xlsx | ForEach-Object {
 }
 
 function export_vba($file_name){
+    $file_path = Join-Path $PWD $file_name
+
+    echo $file_path
+    
+    $excel = new-object -ComObject Excel.Application
+
+    $excel.Workbooks.Open($file_path) | % {
+        $_.VBProject.VBComponents | % {
+
+        }
+    }
+
+}
 
 
+function create_export_file_name($vb_component) {
+
+    switch($vb_component.Type) 
+    {
+        1{ $extension = ".bas"}
+        2{ $extension = ".cls"}
+        3{ $extension = ".frm"}
+        11 { $extension = "" }
+        100{ $extension = ".bas"}
+    }
+
+
+    return [string]::Join($vb_component.Name, $extension)
 }
