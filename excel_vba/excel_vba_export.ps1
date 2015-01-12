1a3f2ea006f837ea217455e436c4012faa69249b
djyugg <sv03@n741x1.com>
2015-01-12 19:11:28 +0900

    typoと変数名の変更

--- excel_vba/excel_vba_export.ps1
+++ excel_vba/excel_vba_export.ps1
@@ -5,14 +5,17 @@ Get-ChildItem -Recurse -Include *.xls,*.xlsx | ForEach-Object {
 }
 
 function export_vba($file_name){
-    create_export_directory $file_name
+    $root_directory_name = [System.IO.Path]::GetFileNameWithoutExtension($file_name)
+    $root_directory_path = Join-Path $PWD $root_directory_name
+
+    create_export_directory $root_directory_path
 
     $excel = new-object -ComObject Excel.Application
     $file_path = Join-Path $PWD $file_name
 
     $excel.Workbooks.Open($file_path) | % {
         $_.VBProject.VBComponents | % {
-            $export_file_path = create_export_file_name $_ $file_name
+            $export_file_path = create_export_file_name $_ $root_directory_path
 
             $_.Export($export_file_path)
         }
@@ -21,15 +24,12 @@ function export_vba($file_name){
     $excel.Quit()
 }
 
-function create_export_directory($file_name) {
-    $extension_exclude_name =[System.IO.Path]::GetFileNameWithoutExtension($file_name)
-    $file_path = Join-Path $PWD $extension_exclude_name
-
+function create_export_directory($root_directory_path) {
     # TODO ActiveX Object
     $sub_directories = @("module", "class", "form", "document_module")
 
     foreach($sub_directory in $sub_directories) {
-        $create_directory_path = Join-Path $file_path $sub_directory
+        $create_directory_path = Join-Path $root_directory_path $sub_directory
 
         create_directory $create_directory_path
     }
@@ -42,38 +42,36 @@ function create_directory($directory_path) {
     }
 }
 
-function create_export_file_name($vb_component, $export_target_file) {
+function create_export_file_name($vb_component, $root_directory_path) {
 
     switch($vb_component.Type)
     {
         1{
             $extension = ".bas"
-            $directory = "module"
+            $sub_directory = "module"
         }
         2{
             $extension = ".cls"
-            $directory = "class"
+            $sub_directory = "class"
         }
         3{
             $extension = ".frm"
-            $directory = "form"
+            $sub_directory = "form"
         }
         # TODO ActiveX Object
         11{
             $extension = ""
-            $directory = "module"
+            $sub_directory = "module"
         }
         100{
             $extension = ".cls"
-            $directory = "dosument_module"
+            $sub_directory = "document_module"
         }
     }
-    $root_directory = [System.IO.Path]::GetFileNameWithoutExtension($export_target_file)
 
-    $create_directory = Join-Path $PWD $root_directory
-    $create_directory = Join-Path $create_directory $directory
-    $file_name = $vb_component.Name + $extension
-    $file_path = Join-Path $create_directory $file_name
+    $create_directory = Join-Path $root_directory_path $sub_directory
+    $export_file_name = $vb_component.Name + $extension
+    $export_file_path = Join-Path $create_directory $export_file_name
 
-    return $file_path
+    return $export_file_path
 }
